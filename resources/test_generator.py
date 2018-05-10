33df0eed9276c1f8d6bbb514fd885f495425dbd5
alex <lebedev.alex69@gmail.com>
2018-05-10 04:51:53 +0300

    commented to build the app

--- resources/test_generator.py
+++ resources/test_generator.py
@@ -1,20 +1,25 @@
 # coding=utf-8
 
-import json
 
+def generate_test():
+    pass
 
-class Test(Resource):
-    def get(self):
-        # just open the file...
-        # read the file and decode possible UTF-8 signature at the beginning
-        # which can be the case in some files.
-        with open("resources/ontology.json", 'r', encoding='utf8') as json_file:
-            # str -> dict
-            data = json.load(json_file)
-
-        # return json.dumps(data, encoding='utf-8')
-        return json.dumps(data, ensure_ascii=False)
-        # return data
-
-    def post(self):
-        pass
+#
+# import json
+#
+#
+# class Test(Resource):
+#     def get(self):
+#         # just open the file...
+#         # read the file and decode possible UTF-8 signature at the beginning
+#         # which can be the case in some files.
+#         with open("resources/ontology.json", 'r', encoding='utf8') as json_file:
+#             # str -> dict
+#             data = json.load(json_file)
+#
+#         # return json.dumps(data, encoding='utf-8')
+#         return json.dumps(data, ensure_ascii=False)
+#         # return data
+#
+#     def post(self):
+#         pass
