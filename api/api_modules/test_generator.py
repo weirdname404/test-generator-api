4772a28c3f4c8e8945e43b95b917bf2aeb5ba804
alex <lebedev.alex69@gmail.com>
2018-05-15 03:25:38 +0300

    test-generation: WIP

--- api/api_modules/test_generator.py
+++ api/api_modules/test_generator.py
@@ -1,25 +1,32 @@
 # coding=utf-8
+import random
+import uuid
+
+from api.ontology_db.db_config.base import Base, Session, engine
+from api.ontology_db.entities.entity_class import EntityClass
+from api.ontology_db.entities.scales import Scale
+from api.ontology_db.entities.steel import Steel
+
+Base.metadata.create_all(engine)
+session = Session()
 
 
-def generate_test():
-    pass
+def generate_test(n, question_type, answer_form, entity_1, entity_2):
+    Base.metadata.create_all(engine)
+    session = Session()
 
-#
-# import json
-#
-#
-# class Test(Resource):
-#     def get(self):
-#         # just open the file...
-#         # read the file and decode possible UTF-8 signature at the beginning
-#         # which can be the case in some files.
-#         with open("resources/ontology.json", 'r', encoding='utf8') as json_file:
-#             # str -> dict
-#             data = json.load(json_file)
-#
-#         # return json.dumps(data, encoding='utf-8')
-#         return json.dumps(data, ensure_ascii=False)
-#         # return data
-#
-#     def post(self):
-#         pass
+    tests = []
+
+    for i in range(len(n)):
+        test = {"guid": str(uuid.uuid4()).upper()}
+        question_type = random.choice(question_type)
+        answer_form = random.choice(answer_form)
+        test["question_type"] = question_type
+        test["answer_form"] = answer_form
+
+        entity_1 = random.choice(entity_1)
+        entity_2 = random.choice(entity_2)
+
+    session.close()
+
+    return tests
