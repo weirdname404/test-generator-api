9f1878464a387257f3465ff4f0595a378f8164a1
alex <lebedev.alex69@gmail.com>
2018-05-18 02:41:55 +0300

    added wrong entities handling

--- api/app.py
+++ api/app.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 import markdown
 from flask import Flask
-from flask import Markup
 from flask import jsonify, request
 from flask_cors import CORS
 
@@ -19,6 +18,13 @@ CORS(app)
 
 # if needed, generate database schema
 Base.metadata.create_all(engine)
+
+session = Session()
+# fetching entity names from the database
+steels = [steel.name for steel in session.query(Steel).all()]
+scales = [scale.name for scale in session.query(Scale).all()]
+classes = [entity_class.name for entity_class in session.query(EntityClass).distinct().all()]
+session.close()
 
 sample = {"questions": [{
     "guid": "Q1_GUID",
@@ -45,7 +51,6 @@ def get_ontology():
 
 @app.route("/drop-ontology")
 def drop_ontology():
-    session = Session()
     drop_tables(session)
     return "DB was dropped.\n"
 
@@ -53,38 +58,28 @@ def drop_ontology():
 # GET method for fetching all entities (classes, objects, scales)
 @app.route('/entities')
 def get_entities():
-    # fetching from the database
-    session = Session()
-    steel_objs = session.query(Steel).all()
-    scale_objs = session.query(Scale).all()
-    class_objs = session.query(EntityClass).distinct().all()
-    session.close()
-
-    objects = [steel.name for steel in steel_objs]
-    scales = [scale.name for scale in scale_objs]
-    classes = [entity_class.name for entity_class in class_objs]
-
-    entities = {'Objects': objects, 'Attributes': scales, 'Classes': classes}
-
-    return jsonify(entities)
+    return jsonify({'Objects': steels, 'Attributes': scales, 'Classes': classes})
 
 
 # Welcome message on test-generator-api.herokuapp.com
 @app.route('/')
 def hello():
     content = """
-    Welcome to test generation API.
-    ==============================
-    Main commands:
-    --------------
-    /entities - GET method for fetching all classes, objects, and attributes.
-    /generate-test - POST method used for test generation. 
-
-    Make sure to have the following structure.
-    curl -XPOST -H "Content-type: application/json" -d '{"key1": "Value1"}' 'https://test-generator-api.herokuapp.com/generate-test'
+    <h3>Welcome to test generation API</h3>
+    <p><b>Main commands:</b><br><br>
+    /entities - <i>GET method for fetching all classes, objects, and attributes.</i><br>
+    /generate-test - <i>POST method which is used to send a request for test generation.</i></p>
+    <b>Make sure to have the following structure of a request:</b>
+    <code> curl -XPOST -H "Content-type: application/json" -d '{"key1": "Value1"}' 
+    'https://test-generator-api.herokuapp.com/generate-test'</code><br><br><br>
+    <b>JSON structure should be similar to the example below:</b><br><br>
+     <code>{"author": "Author","creation_date": "01.01.1970",</code><br>
+     <code>"version": "1.00","guid": "2E7E22F4-5B51-4948-B120-AB21F08317DB",</code><br>
+     <code>"test_requirements": {"count": 5,"question_type": ["O>A"],</code><br>
+     <code>"answer_form": ["binary"],"entities1": ["Ст4сп", "05кп"],"entities2": 
+     ["Максимальная доля углерода в процентах", "ГОСТ сплава"]}}</code><br>
     """
-
-    return Markup(markdown.markdown(content))
+    return content
 
 
 # POST method for test generation
@@ -95,11 +90,17 @@ def generate_test():
         api_response = {'request_info': request.get_json()}
         test_requirements = api_response['request_info']['test_requirements']
         amount = test_requirements['count']
-        if amount < 0: raise ValueError
+        if amount < 0:
+            raise ValueError
+
         request_question_type = test_requirements['question_type']
         request_answer_form = test_requirements['answer_form']
         request_entities1 = test_requirements['entities1']
         request_entities2 = test_requirements['entities2']
+        error_value = ''
+        error_value = check_entities(request_entities1, request_entities2)
+        if error_value:
+            raise TypeError
 
         app.logger.info("\nArguments: %s %s %s %s %s" % (
             amount, request_question_type, request_answer_form, request_entities1, request_entities2))
@@ -110,14 +111,24 @@ def generate_test():
     except ValueError as e:
         return '\nThe amount of questions cannot be < 0\n', 400
 
-    except TypeError as e:
-        return '\nThe request is invalid. %s\n' % e, 400
-
+    except TypeError:
+        return '\nThe request is invalid. %s does not exist in the ontology\n' % (error_value), 400
 
     api_response['questions'] = generate_tests(amount, request_question_type, request_answer_form, request_entities1,
                                                request_entities2)
 
     return jsonify(api_response)
+
+
+# the user could make a mistake so we should check before generation
+def check_entities(request_entities1, request_entities2):
+    all_entities = steels + scales + classes
+    request_entities = request_entities1 + request_entities2
+    print(all_entities, request_entities)
+    for entity in request_entities:
+        if entity not in all_entities:
+            return entity
+    return False
 
 
 if __name__ == '__main__':
