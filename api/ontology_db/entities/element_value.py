1e945bf56af8e07aef9528f86d245dadfea142c5
alex <lebedev.alex69@gmail.com>
2018-05-21 03:29:09 +0300

    refactoring

--- api/ontology_db/entities/element_value.py
+++ api/ontology_db/entities/element_value.py
@@ -14,3 +14,14 @@ class MinCarbonValue(Base):
 
     def __init__(self, value):
         self.value = value
+
+
+# Attribute
+class MaxCarbonValue(Base):
+    __tablename__ = 'max_carbon_values'
+
+    id = Column(Integer, primary_key=True)
+    value = Column(String(20))
+
+    def __init__(self, value):
+        self.value = value
