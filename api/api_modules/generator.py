983b52e2e2ea54f16b13299404a5d0be4468ae16
alex <lebedev.alex69@gmail.com>
2018-05-26 21:03:33 +0300

    minor refactoring; A>O test item generation with all answer types support is added

--- api/api_modules/generator.py
+++ api/api_modules/generator.py
@@ -5,6 +5,8 @@ import uuid
 from api.ontology_db.db_config.base import Base, Session, engine
 from api.ontology_db.entities.scales import Scale
 from api.ontology_db.entities.steel import Steel
+from api.api_modules.generator_modules.stem_generator import generate_stem
+from api.api_modules.generator_modules.generation_config import Config
 
 # connect to DB
 Base.metadata.create_all(engine)
@@ -14,29 +16,9 @@ session = Session()
 ALL_STEELS = session.query(Steel).all()
 ALL_SCALES = session.query(Scale).all()
 
-
-# generator func takes lots of args, that's why special config class was created
-class Config:
-    q_type = ''
-    a_form = ''
-    obj = Steel
-    attr = Scale
-    key_scale_value = []
-    key_object_value = []
-    object_distractors = []
-    scale_value_distractors = []
-    positive = True
-    answer_option = ''
-    obj2 = Steel
-    attr2 = Scale
-    final_key = []
-    final_distractors = []
-
-    def __init__(self, q_type, a_form, ent1, ent2):
-        self.q_type = q_type
-        self.a_form = a_form
-        self.obj = ent1
-        self.attr = ent2
+SA_OBJ = 'steel_attr_obj'
+SA_CLASS = 'steel_attr_class'
+KEY = 'key'
 
 
 # main generator func
@@ -75,10 +57,10 @@ def make_random_choice(test_requirements):
 
 # define the Steel and Scale objects
 def define_object_attribute(steel_object, attribute):
-    steel_object = session.query(Steel).filter(Steel.name == steel_object).all()
-    attribute = session.query(Scale).filter(Scale.name == attribute).all()
+    steel_object = session.query(Steel).filter(Steel.name == steel_object).first()
+    attribute = session.query(Scale).filter(Scale.name == attribute).first()
 
-    return steel_object[0], attribute[0]
+    return steel_object, attribute
 
 
 # define a response-JSON variables
@@ -120,21 +102,40 @@ def choose_entity3(test, entities_list, entity):
     return random_chosen_entity, test
 
 
-# defining [correct attribute value(s)] for a steel object
-def generate_key(steel_object, attribute_name):
+# attribute value, attribute class, steel.attribute object definition
+def define(request_code, steel_object, attribute_name):
     if 'в процентах' in attribute_name:
         attribute_name = attribute_name.split()[2]
 
-    relations_dict = {
-        'Способ раскисления': [steel_object.deoxidizing_type.name],
-        'Содержание легирующих элементов': [i.name for i in steel_object.alloying_elements],
-        'Характеристика качества': [steel_object.quality.name],
-        'ГОСТ сплава': [steel_object.gost.name],
-        'углерода': [steel_object.min_carbon_value.value + '-' + steel_object.max_carbon_value.value],
-        'марганца': [steel_object.min_marganese_value.value + '-' + steel_object.max_marganese_value.value]
+    # Classes and Objects
+    # {attribute_name: [attributeClass, Steel.attribute, [steel_obj.attribute.value]}
+    attribute_dict = {
+        'Способ раскисления': [steel_object.deoxidizing_type,
+                               Steel.deoxidizing_type, [steel_object.deoxidizing_type.name]],
+
+        'Содержание легирующих элементов': [steel_object.alloying_elements, Steel.alloying_elements,
+                                            [i.name for i in steel_object.alloying_elements]],
+
+        'Характеристика качества': [steel_object.quality, Steel.quality, [steel_object.quality.name]],
+        'ГОСТ сплава': [steel_object.gost, Steel.gost, [steel_object.gost.name]],
+
+        'углерода': [steel_object.min_carbon_value, Steel.min_carbon_value,
+                     [steel_object.min_carbon_value.value + '-' + steel_object.max_carbon_value.value]],
+
+        'марганца': [steel_object.min_marganese_value, Steel.min_marganese_value,
+                     [steel_object.min_marganese_value.value + '-' + steel_object.max_marganese_value.value]]
     }
 
-    return relations_dict[attribute_name]
+    # defining attribute class
+    # defining steel.attribute object
+    # defining [correct attribute value(s)] for a steel object
+    response = {
+        SA_OBJ: attribute_dict[attribute_name][0],
+        SA_CLASS: attribute_dict[attribute_name][1],
+        KEY: attribute_dict[attribute_name][2]
+    }
+
+    return response[request_code]
 
 
 # combine min and max values of the element to create [min1-max1, min2-max2, ...] distractors if it is needed
@@ -154,16 +155,19 @@ def generate_min_max_distractors(distractors, attribute):
 
 # generating a stem, distractors and keys
 def generate_test_item(config):
-    config.key_scale_value = generate_key(config.obj, config.attr.name)
-    binary = True if config.a_form == 'binary' else False
-    stem = ''
+    config.key_scale_value = define(KEY, config.obj, config.attr.name)
+
+    if config.a_form == 'binary':
+        config.final_distractors = ['Да', 'Нет']
+
+        if (config.q_type == 'O>A' or config.q_type == 'A>O') and shorten_generation_process(config):
+            return generate_stem(config), config.final_distractors, config.final_key
 
     if config.q_type == 'O>A':
         generate_oa_distractors_and_answers(config)
-        stem = generate_oa_stem(binary, config.obj, config.attr, config.answer_option, stem)
 
     elif config.q_type == 'A>O':
-        pass
+        generate_ao_distractors_and_answers(config)
 
     elif config.q_type == 'O>A>O':
         # getting all Steels objects except initial Steel object; shuffling
@@ -175,33 +179,13 @@ def generate_test_item(config):
     elif config.q_type == 'A>O>A':
         pass
 
-    return stem, config.final_distractors, config.final_key
+    return generate_stem(config), config.final_distractors, config.final_key
 
 
 # All answer forms for O>A
 def generate_oa_distractors_and_answers(config):
     steel_object, attribute, key_scale_value = config.obj, config.attr, config.key_scale_value
-    # Case to shorten the generation process
-    if config.a_form == 'binary':
-        config.final_distractors = ['Да', 'Нет']
-        if config.positive:
-            config.answer_option = random.choice(config.key_scale_value).lower()
-            config.final_key = ['Да']
-            return
-
-    # Fetching all steel objects except initial steel object OR getting all values from chosen Scale.
-    # The key aka correct answer(s) is removed from distractors to avoid random choice uncertainty.
-    distractors = session.query(Steel).filter(
-        Steel.name != steel_object.name,
-        Steel.min_carbon_value != steel_object.min_carbon_value).all() if 'в процентах' in attribute.name \
-        else list({i.value for i in attribute.values} - set(key_scale_value))
-
-    """ 
-    [i.value for i in attribute.values]
-    This approach is an appropriate way to work with 
-    ALL distractors including not existing values and excluding the right answer.
-    If we just randomly choose from other objects' values, we will miss a great opportunity to use fake values.
-    """
+    distractors = generate_scale_values_distractors(config)
 
     if config.a_form == 'binary':
         config.final_key = ['Нет']
@@ -214,51 +198,78 @@ def generate_oa_distractors_and_answers(config):
         config.scale_value_distractors = generate_min_max_distractors(distractors, attribute)
         config.final_key = config.key_scale_value if config.a_form == 'options' \
             else [random.choice(config.key_scale_value)]
+
         config.final_distractors = config.final_key + config.scale_value_distractors
 
 
-def generate_oa_stem(binary, steel_object, attribute, answer_option, stem):
-    if binary:
-        if attribute.name == 'Способ раскисления':
-            stem += f"Верно ли, что сталь {steel_object.name} по способу раскисления {answer_option}?"
+# For binary question with positive question we need everything we need, so the generation is pointless
+def shorten_generation_process(config):
+    if config.a_form == 'binary':
+        if config.positive:
+            config.answer_option = random.choice(config.key_scale_value).lower()
+            config.final_key = ['Да']
+            return True
+    return False
 
-        elif attribute.name == 'Содержание легирующих элементов':
-            stem += f"Содержит ли сталь {steel_object.name} {answer_option} в качестве легирующего элемента?"
 
-        elif attribute.name == 'Характеристика качества':
-            stem += f"Верно ли, что сталь {steel_object.name} {answer_option}?"
+def generate_scale_values_distractors(config):
+    """
+    [i.value for i in attribute.values]
+    This approach is an appropriate way to work with
+    ALL distractors including not existing values and excluding the right answer.
+    If we just randomly choose from other objects' values, we will miss a great opportunity to use fake values.
+    """
+    # defining DB steel object and steel object from request JSON
+    db_steel_attribute_value = define(SA_CLASS, config.obj, config.attr.name)
+    requested_steel_attribute_value = define(SA_OBJ, config.obj, config.attr.name)
+    # Fetching all steel objects except initial steel object OR getting all values from chosen Scale.
+    # The key aka correct answer(s) is removed from distractors to avoid random choice uncertainty.
+    return session.query(Steel) \
+        .filter(Steel.name != config.obj.name,
+                db_steel_attribute_value != requested_steel_attribute_value) \
+        .all() if 'в процентах' in config.attr.name else list(
+        {i.value for i in config.attr.values} - set(config.key_scale_value))
 
-        elif attribute.name == 'ГОСТ сплава':
-            stem += f"Входит ли сталь {steel_object.name} в {answer_option.upper()}?"
 
-        else:
-            stem += f"Верно ли, что массовая доля {attribute.name.split()[2]} у стали " + \
-                    f"{steel_object.name} равна {answer_option}%?"
+# All answer forms for A>O
+def generate_ao_distractors_and_answers(config):
+    steel_object, attribute, key_scale_value = config.obj, config.attr, config.key_scale_value
+    distractors = generate_scale_values_distractors(config)
 
-    # Options or Choice
+    if config.a_form == 'binary':
+        config.final_key = ['Нет']
+        # key is a list, we can handle single and multiple answers
+        config.answer_option = random.choice(generate_min_max_distractors(distractors, attribute)).lower()
+
     else:
-        if attribute.name == 'Способ раскисления':
-            stem += f"Охарактеризуйте с точки зрения способа раскисления сталь {steel_object.name}:"
-
-        elif attribute.name == 'Содержание легирующих элементов':
-            stem += f"{generate_synonym('verb')} один или несколько легирующих элементов, которые " + \
-                    f"входят в состав стали {steel_object.name}:"
-
-        elif attribute.name == 'Характеристика качества':
-            stem += f"Сталь {steel_object.name} можно охарактеризовать с точки зрения качества как:"
-
-        elif attribute.name == 'ГОСТ сплава':
-            stem += f"{generate_synonym('verb')} к какому ГОСТу относится сталь {steel_object.name}:"
+        steel_attribute_obj = define(SA_OBJ, config.obj, config.attr.name)
+        db_steel_attribute_obj = define(SA_CLASS, config.obj, config.attr.name)
+        config.answer_option = ', '.join(i for i in config.key_scale_value)
+        if 'легир' in config.attr.name:
+            object_distractors = [i for i in ALL_STEELS if i.alloying_elements != steel_attribute_obj]
+            object_possible_keys = [i for i in ALL_STEELS if
+                                    i.alloying_elements == steel_attribute_obj and i.name != config.obj.name]
 
         else:
-            stem += f"{generate_synonym('verb')} какая массовая доля {attribute.name.split()[2]} в процентах у " + \
-                    f"стали {steel_object.name}:"
+            # fetch distracting objects
+            object_distractors = session.query(Steel) \
+                .filter(db_steel_attribute_obj != steel_attribute_obj) \
+                .all()
 
-    return stem
+            # fetch valid objects
+            object_possible_keys = session.query(Steel) \
+                .filter(db_steel_attribute_obj == steel_attribute_obj, Steel.name != config.obj.name) \
+                .all()
 
+        if config.a_form == "choice":
+            config.final_key = [config.obj.name]
+            random.shuffle(object_distractors)
+            config.final_distractors = config.final_key + [steel.name for steel in object_distractors[:4]]
 
-def generate_synonym(word):
-    verb = ['Определите', 'Укажите', 'Выберите', 'Назовите']
-    adjective = []
-    word_dict = {'verb': verb, 'adj': adjective}
-    return random.choice(word_dict[word])
+        else:
+            distractor_num = random.randint(0, 4)
+            random.shuffle(object_distractors)
+            random.shuffle(object_possible_keys)
+            distractors = [steel.name for steel in object_distractors[:distractor_num]]
+            config.final_key = [steel_object.name] + [steel.name for steel in object_possible_keys[:4 - distractor_num]]
+            config.final_distractors = config.final_key + distractors
