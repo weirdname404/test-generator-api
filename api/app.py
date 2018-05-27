61e78c76696c6ea06f56bc42b818bcfe6cd49f1a
alex <lebedev.alex69@gmail.com>
2018-05-27 23:09:25 +0300

    welcome page updated

--- api/app.py
+++ api/app.py
@@ -27,6 +27,15 @@ scales = [scale.name for scale in session.query(Scale).all()]
 classes = [entity_class.name for entity_class in session.query(EntityClass).distinct().all()]
 session.close()
 
+last_successful_request = """
+curl -XPOST -H "Content-type: application/json" -d '{"author": 
+"Author","creation_date": "01.01.1970","version": "1.00","guid": "2E7E22F4-5B51-4948-B120-AB21F08317DB",
+"test_requirements": {"count": 100,"question_type": ["O>A>O", "O>A", "A>O"],
+"answer_form": ["binary", "choice", "options"],"entities1": ["Ст4сп", "05кп", "50", "20", "60"],
+"entities2": ["Содержание легирующих элементов", "Способ раскисления", 
+"Характеристика качества", "ГОСТ сплава", "Максимальная доля углерода в процентах", 
+"Минимальная доля марганца в процентах"]}}' 'test-generator-api.herokuapp.com/generate-test'"""
+
 
 @app.route("/update-ontology")
 def get_ontology():
@@ -49,19 +58,17 @@ def get_entities():
 # Welcome message on test-generator-api.herokuapp.com
 @app.route('/')
 def hello():
-    return """<h3>Welcome to test generation API</h3>
-    <p><b>Main commands:</b><br><br>
+    return """<h3>Welcome to Educational Test Item Generation API</h3>
+    <p><b>Main commands:</b><br>
     /entities - <i>GET method for fetching all classes, objects, and attributes.</i><br>
-    /generate-test - <i>POST method which is used to send a request for test generation.</i></p>
-    <b>Make sure to have the following structure of a request:</b>
-    <code> curl -XPOST -H "Content-type: application/json" -d '{"key1": "Value1"}' 
-    'https://test-generator-api.herokuapp.com/generate-test'</code><br><br><br>
-    <b>JSON structure should be similar to the example below:</b><br><br>
+    /generate-test - <i>POST method which is used to send a request for test generation.</i></p><br>
+    <b>JSON structure should be similar to the example below:</b><br>
      <code>{"author": "Author","creation_date": "01.01.1970",</code><br>
      <code>"version": "1.00","guid": "2E7E22F4-5B51-4948-B120-AB21F08317DB",</code><br>
      <code>"test_requirements": {"count": 5,"question_type": ["O>A"],</code><br>
      <code>"answer_form": ["binary"],"entities1": ["Ст4сп", "05кп"],"entities2": 
-     ["Максимальная доля углерода в процентах", "ГОСТ сплава"]}}</code><br>"""
+     ["Максимальная доля углерода в процентах", "ГОСТ сплава"]}}</code><br><br><br>
+     <b>Last successful request via terminal:</b><br><code> %s </code><br><br>""" % last_successful_request
 
 
 # POST method for test generation
